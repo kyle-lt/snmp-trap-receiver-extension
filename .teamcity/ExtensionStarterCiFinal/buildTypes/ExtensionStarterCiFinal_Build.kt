ed2fb2546d5ea63ee8fbf36b1b8f75ab3a5b6f9c
ajagtiani <ajagtiani@TeamCity>
2019-02-26 13:29:58 -0800

    TeamCity change in 'Extension Starter Ci Final' project: 1 build step(s) were added to 'Build' build configuration

--- .teamcity/ExtensionStarterCiFinal/buildTypes/ExtensionStarterCiFinal_Build.kt
+++ .teamcity/ExtensionStarterCiFinal/buildTypes/ExtensionStarterCiFinal_Build.kt
@@ -1,6 +1,7 @@
 package ExtensionStarterCiFinal.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.maven
 import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.vcs
 
 object ExtensionStarterCiFinal_Build : BuildType({
@@ -8,6 +9,15 @@ object ExtensionStarterCiFinal_Build : BuildType({
 
     vcs {
         root(ExtensionStarterCiFinal.vcsRoots.ExtensionStarterCiFinal_HttpsGithubComAdityajagtiani89extensionStarterCiRefsHead)
+    }
+
+    steps {
+        maven {
+            goals = "clean test"
+            pomLocation = ".teamcity/pom.xml"
+            runnerArgs = "-Dmaven.test.failure.ignore=true"
+            mavenVersion = defaultProvidedVersion()
+        }
     }
 
     triggers {
