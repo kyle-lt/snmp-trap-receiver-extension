2dff0ad7a6332089fe13e9a07a246916dea69b3f
aditya.jagtiani <aditya.jagtiani@TeamCity>
2019-03-08 19:19:27 +0000

    TeamCity change in 'Extension Starter' project: project parameters were changed

--- .teamcity/ExtensionStarter/patches/projects/85c0e0f7-336b-42d1-84ad-8abe32b64865.kts
+++ .teamcity/ExtensionStarter/patches/projects/85c0e0f7-336b-42d1-84ad-8abe32b64865.kts
@@ -14,5 +14,8 @@ changeProject(uuid("85c0e0f7-336b-42d1-84ad-8abe32b64865")) {
         add {
             param("env.GLOBAL_ACCOUNT_NAME", "customer1_70a0b3c7-3f29-4b30-afea-f6e173520cd0")
         }
+        add {
+            param("env.EVENTS_SERVICE_HOST", "ec2-34-221-206-45.us-west-2.compute.amazonaws.com")
+        }
     }
 }
