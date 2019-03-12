1b94045f41433bb6db0628c5e8ebc23b2da0b6ce
VishakaSekar <vishaka.sekar@appdynamics.com>
2019-03-12 11:27:46 -0700

    removing artifactRules from Build

--- .teamcity/ExtensionStarter/buildTypes/ExtensionStarter_Build.kt
+++ .teamcity/ExtensionStarter/buildTypes/ExtensionStarter_Build.kt
@@ -13,7 +13,7 @@ object ExtensionStarter_Build : BuildType({
     uuid = "75c498f0-14c7-45a6-a538-53ad0f9b87dc"
     name = "Build"
 
-    artifactRules = "target/ExtensionStarterCiMonitor-*.zip"
+    //artifactRules = "target/ExtensionStarterCiMonitor-*.zip"
 
     vcs {
         root(ExtensionStarter_GitGithubComAdityajagtiani89extensionStarterCiGit)
