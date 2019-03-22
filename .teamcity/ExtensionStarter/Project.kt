0cc745e69754fd8266a995560791d618d46451b3
VishakaSekar <vishaka.sekar@appdynamics.com>
2019-03-22 10:43:29 -0700

    escaping $ with $$  in key

--- .teamcity/ExtensionStarter/Project.kt
+++ .teamcity/ExtensionStarter/Project.kt
@@ -23,7 +23,7 @@ object Project : Project({
     buildType(ExtensionStarter_Build)
 
     params {
-        password("env.APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY", "zxxae5b77700e4d524b0f17c60975f42d1a")
+        password("env.APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY", "zxxae5b77700e4d524bc1108cb6dbc0df0c")
         param("env.APPDYNAMICS_CONTROLLER_PORT", "8080")
         param("env.APPDYNAMICS_CONTROLLER_HOST_NAME", "172.28.1.1")
         param("env.APPDYNAMICS_AGENT_ACCOUNT_NAME", "customer1")
